1a09ff2d921d6aaf362d6bd74bf2ac9320a89fb7
Konstantin Kharun <konstantin19980209@gmail.com>
2023-03-21 00:39:54 +0300

    Add junit testing

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashBackHackerTest {
+    CashBackHacker service = new CashBackHacker();
+
+    @Test
+    public void checkAmount0() {
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkAmount1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkAmount990() {
+        int actual = service.remain(999);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkAmount1001() {
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void checkAmount3500() {
+        int actual = service.remain(3500);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
